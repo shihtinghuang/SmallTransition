dc5b4b47422a26024359f0b42e2ef9c2986a1277
ShihTing <s-huang@unext.jp>
2020-01-31 09:47:08 +0900

    Initial Commit

---
+++ NarrowScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NarrowScreen
+//
+//  Created by s-huang on 2020/01/31.
+//  Copyright © 2020 U-Next. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
